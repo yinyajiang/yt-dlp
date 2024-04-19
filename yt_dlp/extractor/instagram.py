7746730ed66853df46381f97e5516912020ef522
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-04-19 14:54:52 +0800

    Update instagram.py

--- yt_dlp/extractor/instagram.py
+++ yt_dlp/extractor/instagram.py
@@ -690,8 +690,12 @@ class InstagramUserIE(InstagramPlaylistBaseIE):
             'custom_action', default=[''], ie_key=InstagramUserIE)[0]
         userdata = self._download_json(
             f'{self._API_BASE_URL}/users/web_profile_info/?username={username}&count=100',
-            username, errnote=False, fatal=False, headers=self._API_HEADERS)['data']
-
+            username, errnote=False, fatal=False, headers=self._API_HEADERS)
+        if not userdata:
+            self.report_warning('userdata extraction failed', username)
+            if not self._has_session_id():
+                self.raise_login_required()
+        userdata = userdata['data']
         if action == 'get_post_count':
             return {
                 '_type': 'custom_action',

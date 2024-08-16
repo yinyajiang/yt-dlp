775e844a4439cac6b26a80f5470928bfed78ced7
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-08-16 16:54:26 +0800

    up

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -2431,6 +2431,7 @@ from .vvvvid import (
     VVVVIDIE,
     VVVVIDShowIE,
 )
+from .vyneer import VyneerIE
 from .walla import WallaIE
 from .washingtonpost import (
     WashingtonPostArticleIE,

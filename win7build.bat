ebddc490094b4ef02af4bf246adeb360cbc82165
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2024-10-28 14:10:48 +0800

    Update win7build.bat

--- win7build.bat
+++ win7build.bat
@@ -1,12 +1,21 @@
 REM windows7 python https://github.com/adang1345/PythonWin7/blob/master/3.10.11
 
 @echo off
-set "PYTHON_PATH=%LOCALAPPDATA%\Programs\Python\Python310"
-if not exist "%PYTHON_PATH%" (
-	set "PYTHON_PATH=%APPDATA%\Python\Python310"
+
+if defined ProgramFiles(x86) (
+    echo 64 bit system
+	set "PYTHON_PATH=%LOCALAPPDATA%\Programs\Python\Python310"
+	if not exist "%PYTHON_PATH%" (
+		set "PYTHON_PATH=%APPDATA%\Python\Python310"
+	)
+) else (
+    echo 32 bit system
+	set "PYTHON_PATH=%LOCALAPPDATA%\Programs\Python\Python310-32"
+	if not exist "%PYTHON_PATH%" (
+		set "PYTHON_PATH=%APPDATA%\Python\Python310-32"
+	)
 )
 set "PATH=%PYTHON_PATH%;%PYTHON_PATH%\Scripts;%PATH%"
-
 python -V
 for /f "delims=" %%i in ('python -V 2^>^&1') do set "PYTHON_VERSION=%%i"
 if "%PYTHON_VERSION%" neq "Python 3.10.11" (

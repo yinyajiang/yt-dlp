d3d5d471fffa20634954c086b1e607e3f55eb07d
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-02-21 14:28:31 +0800

    add Locals

--- yt_dlp/extractor/_extractors.py
+++ yt_dlp/extractor/_extractors.py
@@ -1084,6 +1084,7 @@ from .livestream import (
 )
 from .livestreamfails import LivestreamfailsIE
 from .lnk import LnkIE
+from .locals import LocalsIE
 from .loom import (
     LoomFolderIE,
     LoomIE,

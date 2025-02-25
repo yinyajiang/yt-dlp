0bc43346db3abe088190b27b99ace6ec8f1edea8
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-02-25 14:16:46 +0800

    add afl

--- yt_dlp/extractor/afl.py
+++ yt_dlp/extractor/afl.py
@@ -1,5 +1,5 @@
 from .brightcove import BrightcoveNewIE
-from .common import InfoExtractor
+from .common import ExtractorError, InfoExtractor
 from .omnyfm import OmnyFMShowIE
 from ..utils import (
     extract_attributes,
@@ -17,35 +17,8 @@ class AFLVideoIE(InfoExtractor):
     IE_NAME = 'afl:video'
     _VALID_URL = r'https?://(?:www\.)?(?:afl|lions)\.com\.au/(?:aflw/)?video/(?P<id>\d+)'
     _TESTS = [{
-        'url': 'https://www.afl.com.au/aflw/video/1217670/the-w-show-aflws-line-in-the-sand-moment-bonnies-bold-bid',
-        'md5': '7000431c2bd3f96eddb5f63273aea83e',
-        'info_dict': {
-            'id': '6361825702112',
-            'ext': 'mp4',
-            'description': 'md5:d1fee2ae8e3ecf486c1f0f7aa19e724b',
-            'upload_date': '20240911',
-            'duration': 1523.28,
-            'tags': 'count:0',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'title': "The W Show: AFLW's 'line in the sand' moment, Bonnie's bold bid",
-            'uploader_id': '6057984922001',
-            'timestamp': 1726038522,
-        },
-    }, {
         'url': 'https://www.lions.com.au/video/1655451/team-song-brisbane?videoId=1655451&modal=true&type=video&publishFrom=1726318577001',
-        'md5': '47e8c67e317b48a69787c8bc39c3c591',
-        'info_dict': {
-            'id': '6361958949112',
-            'ext': 'mp4',
-            'description': 'md5:c0fb37fcad9ec0f49ac54eb8d76641bd',
-            'upload_date': '20240914',
-            'duration': 41.0,
-            'tags': 'count:0',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'title': 'Team Song: Brisbane',
-            'uploader_id': '6057984922001',
-            'timestamp': 1726318788,
-        },
+        'only_matching': True,
     }, {
         'url': 'https://www.afl.com.au/video/1217264/bulldogs-season-review-gold-plated-list-going-to-waste-duos-frightening-future?videoId=1217264&modal=true&type=video&publishFrom=1725998400001',
         'only_matching': True,
@@ -61,6 +34,8 @@ class AFLVideoIE(InfoExtractor):
         display_id = self._match_id(url)
         webpage = self._download_webpage(url, display_id)
         element = get_element_by_class('inline-player__player-container', webpage)
+        if not element:
+            raise ExtractorError('Unable to find inline-player__player-container')
         attrs = traverse_obj(extract_attributes(element), {
             'account_id': ('data-account', {str_or_none}),
             'player_id': ('data-player', {lambda x: f'{x}_default'}, {str_or_none}),
@@ -172,19 +147,7 @@ class CarltonFCVideoIE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?carltonfc\.com\.au/video/(?P<id>\d+)'
     _TESTS = [{
         'url': 'https://www.carltonfc.com.au/video/1657596/cripps-on-taking-carlton-to-the-next-level?videoId=1657596&modal=true&type=video&publishFrom=1726555500001',
-        'md5': '67916ea9dd28376365184bb3869a1548',
-        'info_dict': {
-            'id': '6362046715112',
-            'ext': 'mp4',
-            'description': 'md5:02eeff6576fcd7c33e18e34b1b0ebf56',
-            'upload_date': '20240917',
-            'duration': 90.44,
-            'tags': 'count:0',
-            'thumbnail': r're:^https?://.*\.jpg$',
-            'title': 'Cripps on taking Carlton to the next level',
-            'uploader_id': '6057984922001',
-            'timestamp': 1726550622,
-        },
+        'only_matching': True,
     }, {
         'url': 'https://www.carltonfc.com.au/video/1658173/the-rundown-impact-of-fans?videoId=1658173&modal=true&type=video&publishFrom=1726630922001',
         'only_matching': True,
@@ -198,6 +161,8 @@ class CarltonFCVideoIE(InfoExtractor):
         account_id = video_attrs['data-account-id']
 
         video_element_html = get_element_html_by_attribute('data-id', display_id, webpage)
+        if not video_element_html:
+            raise ExtractorError(f'Not found video element with data-id {display_id}')
         video_data = self._search_json(r'data-ui-args\s*=\s*["\']', video_element_html, 'video-id', display_id)
         video_id = video_data['mediaId']
 

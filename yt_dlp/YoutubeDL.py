51af7d036d344538d95d41d26bbf1eb5cfb28c4a
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-11-10 17:11:50 +0800

    Update YoutubeDL.py

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -742,9 +742,11 @@ class YoutubeDL:
 
         # Note: this must be after plugins are loaded
         self.params['js_runtimes'] = self.params.get('js_runtimes', {'deno': {}})
-        if self.params.get('js_runtimes') and not self.params.get('js_runtimes').get('deno'):
+        cfg_deno = traverse_obj(self.params, ('js_runtimes', 'deno', 'path'), default=None)
+        if not cfg_deno:
             if os.getenv('DENO_PATH'):
                 self.write_debug(f"Using DENO_PATH environment variable: {os.getenv('DENO_PATH')}")
+                self.params['js_runtimes'] = {'deno': {}}
                 self.params['js_runtimes']['deno'] = {'path': os.getenv('DENO_PATH')}
         self._clean_js_runtimes(self.params['js_runtimes'])
 

52d5bd17f3bffe2bf92144934386c6518d570104
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2025-11-20 16:43:25 +0800

    up

--- yt_dlp/YoutubeDL.py
+++ yt_dlp/YoutubeDL.py
@@ -743,15 +743,12 @@ class YoutubeDL:
 
         # Note: this must be after plugins are loaded
         self.params['js_runtimes'] = self.params.get('js_runtimes', {'deno': {}})
-        cfg_deno = traverse_obj(self.params, ('js_runtimes', 'deno', 'path'), default=None)
-        if not cfg_deno:
-            if os.getenv('DENO_PATH'):
-                self.write_debug(f"Using DENO_PATH environment variable: {os.getenv('DENO_PATH')}")
-                self.params['js_runtimes'] = {'deno': {}}
-                self.params['js_runtimes']['deno'] = {'path': os.getenv('DENO_PATH')}
-
-        if sys.platform.lower() == 'darwin':
-            self.params['js_runtimes'] = {'deno': {}}
+        # cfg_deno = traverse_obj(self.params, ('js_runtimes', 'deno', 'path'), default=None)
+        # if not cfg_deno:
+        #     if os.getenv('DENO_PATH'):
+        #         self.write_debug(f"Using DENO_PATH environment variable: {os.getenv('DENO_PATH')}")
+        #         self.params['js_runtimes'] = {'deno': {}}
+        #         self.params['js_runtimes']['deno'] = {'path': os.getenv('DENO_PATH')}
 
         self._clean_js_runtimes(self.params['js_runtimes'])
 

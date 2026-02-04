a8730880bf65687ce043ad17c3fcb6a0a799d0bb
yinyajiang <32933356+yinyajiang@users.noreply.github.com>
2026-02-04 11:06:38 +0800

    Update common.py

--- yt_dlp/extractor/common.py
+++ yt_dlp/extractor/common.py
@@ -4546,7 +4546,7 @@ class InfoExtractor:
         try:
             return MutilThirdIE(self).extract_video_info(url, video_id)
         except Exception as e:
-            self.report_warning(f'use third api failed: {e}')
+            self.report_warning(f'_extract_use_third_mutil_api failed: {e}')
             return None
 
     @staticmethod
